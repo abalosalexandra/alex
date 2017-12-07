99e806821bee7c4a7f6e056d033fdd33b23461e9
Alexandra Abalos <31023452+abalosalexandra@users.noreply.github.com>
2017-12-07 14:32:39 +0800

    Add files via upload

---
+++ www/js/process.js
@@ -0,0 +1,15 @@
+let process = {
+	menu:function(){
+		let content = 
+		<div id="menu">
+	        <center>
+    		    <h1> Italian App Recipe </h1><hr />
+        			<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+       			<h1><a href="index.html">All Recipes </a></h1>
+			</center>
+		</div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	}
+}
+
+process.menu();
